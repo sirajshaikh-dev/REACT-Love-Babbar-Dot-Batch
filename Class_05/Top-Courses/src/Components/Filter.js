27ffcd8917f1b8307a3612dd6f1f20f716b6ee73
Siraj <ss1425306@gmail.com>
2024-10-24 21:10:39 +0530

    Add active style to btn

--- Class_05/Top-Courses/src/Components/Filter.js
+++ Class_05/Top-Courses/src/Components/Filter.js
@@ -11,7 +11,12 @@ const Filter = ({filterData,category,setCategory}) => {
       {
         filterData.map((data)=>(
           <button
-          className={`text-lg px-2 py-1 rounded-md font-medium text-white bg-black hover:bg-opacity-50 border-2 transition-all duration-300`}
+          className={`text-lg px-2 py-1 rounded-md font-medium text-white bg-black hover:bg-opacity-50 border-2 transition-all duration-300
+            ${category===data.title ?
+              "bg-opacity-60 border-white":
+              "bg-opacity-40 border-transparent"
+            }
+            `}
           key={data.id}
           onClick={( )=> filterHandler(data.title)}
           >{data.title}
