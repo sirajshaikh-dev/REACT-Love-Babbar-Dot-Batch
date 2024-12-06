c61c928a3b31cbc919e6733d8ebf7c9a555ac544
Siraj <ss1425306@gmail.com>
2024-12-06 21:22:45 +0530

    Add cartitem

--- React-Advance/shopping-cart-redux/src/components/CartItem.jsx
+++ React-Advance/shopping-cart-redux/src/components/CartItem.jsx
@@ -1,5 +1,23 @@
-const CartItem = () => {
-  return <div>CartItem</div>;
-};
+ import React from 'react'
+ import { FaCar } from 'react-icons/fa'
+ const CartItem = ({item,itemIndex}) => {
+   return (
+     <div>
+        <div>
 
-export default CartItem;
+          <div>
+            <img src={item.title} alt="" />
+            <h1>{item.description}</h1>
+            <div>
+              <p>{item.price}</p>
+              <div>
+                <FaCar/>
+              </div>
+            </div>
+          </div>
+        </div>
+     </div>
+   )
+ }
+ 
+ export default CartItem
